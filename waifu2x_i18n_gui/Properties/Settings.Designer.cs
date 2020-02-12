8b60c319e9fc2e6e3fbd1f480b96b2279adbe88b
fg118942 <ekusuy-s@jcom.home.ne.jp>
2020-02-12 21:07:03 +0900

    二重拡張子を防止する設定をデフォルトに変更

--- waifu2x_i18n_gui/Properties/Settings.Designer.cs
+++ waifu2x_i18n_gui/Properties/Settings.Designer.cs
@@ -169,7 +169,7 @@ namespace waifu2x_ncnn_vulkan_gui.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool Prevent_double_extensions {
             get {
                 return ((bool)(this["Prevent_double_extensions"]));
